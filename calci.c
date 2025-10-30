da35624f21cfd54ac457be1ff679bc9afbfb40a6
Dhananjaya Roul <dhananjaya032007@gmail.com>
2025-10-30 15:38:14 +0530

    Add files via upload

---
+++ calci.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int main() {
+    float num1,num2,result;
+    char op;
+    printf("Enter an operator (+, -, *, /): ");
+    scanf(" %c",&op);
+    printf("Enter two numbers:");
+    scanf("%f %f",&num1,&num2);
+    switch(op) {
+        case'+':
+            result=num1+num2;
+            printf("Result=%.2f\n",result);
+            break;
+        case'-':
+            result=num1-num2;
+            printf("Result=%.2f\n",result);
+            break;
+        case'*':
+            result=num1*num2;
+            printf("Result=%.2f\n",result);
+            break;
+        case'/':
+            if(num2==0) {
+                printf("Error! Division by zero.\n");
+            } else {
+                result=num1/num2;
+                printf("Result=%.2f\n",result);
+            }
+            break;
+            default:
+            printf("Invalid operator\n");
+        }
+        return 0;
+    }
