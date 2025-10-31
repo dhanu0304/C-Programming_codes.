3665f5e8545403b081e9df0c3d81e5e328523bb0
Dhananjaya Roul <dhananjaya032007@gmail.com>
2025-10-31 15:34:06 +0530

    Add files via upload

---
+++ multipletable.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    int a, i;
+    printf("Enter an integer: ");
+    scanf("%d", &a);
+    for (i = 1; i <= 10; i++)
+    {
+        printf("%d x %d = %d\n", a, i, a * i);
+    }
+    return 0;
+}
